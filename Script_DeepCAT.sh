cb9d09bdaa11f1dfeab2dd7ffa88c0c27a4a9416
Daria-cloud <dasha_beshnova@mail.ru>
2019-12-06 15:17:19 -0600

    Script

---
+++ Script_DeepCAT.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -f
+# If raw TCR repertoire sequencing data are available please place data 
+# in the "Input" folder
+var1="Output"
+# If raw TCR repertoire sequencing data are not available 
+# please use our Sample Data as an example 
+#python DeepCAT_modif.py $var3  $var4   
+var2="iSMART_results"
+var3="DeepCAT_CHKP"
+#
+args=("$@")
+if [ ${args[0]} == '-r' ]; then
+  if [ "$(ls -A ${args[1]})" ]; then
+     if [ ! -d $var2 ]; then
+        mkdir $var2
+     fi
+     python PrepareAdaptiveFile.py ${args[1]} $var1
+     python iSMARTm.py -d $var1 -o $var2  
+  else
+     echo "Error! The" ${args[1]} "directory is empty"
+     exit 1
+  fi   
+elif [ ${args[0]} == '-p' ]; then
+    echo "The files in SampleData will be processed"
+    var1="SampleData"
+    if [ ! -d $var2 ]; then
+      mkdir $var2
+    fi
+    python iSMARTm.py -d $var1 -o $var2
+fi  
+python DeepCAT_modif.py $var2  $var3
